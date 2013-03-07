b751af21d573914af8fa73303d9e52ebb0c23d5f
Michael Witrant <michael@ouvrages-web.fr>
2013-03-07 12:28:37 +0100

    fixed default ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     # See the wiki for details:
     # https://github.com/ryanb/cancan/wiki/Defining-Abilities
-    if user and user.has_role? :admin
+    if user
       can :manage, :all
     else
       can :read, :all
